16b51afbe64f87084dd2efd7d8cf5e56d6edec89
Karasz Máté <k6mate@gmail.com>
2025-11-25 21:06:22 +0100

    11/25/2025 - dsadsa

---
+++ 17 - IO feladatok/13 - Snooker/Snooker/Snooker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Snooker;
+
+// 2. feladat: Adatok beolvasása
+List<Snooker.Snooker> versenyzok = new List<Snooker.Snooker>();
+string[] sorok = File.ReadAllLines(@"c:\Users\Admin\Desktop\github\13-Asztali-Alkalmazasok\17 - IO feladatok\13 - Snooker\snooker.txt", Encoding.UTF8);
+
+for (int i = 1; i < sorok.Length; i++)
+{
+    versenyzok.Add(new Snooker.Snooker(sorok[i]));
+}
+
+// 3. feladat: Versenyzők száma
+Console.WriteLine($"3. feladat: A világranglistán {versenyzok.Count} versenyző szerepel.");
+
+// 4. feladat: Átlagos bevétel
+double atlag = versenyzok.Average(v => v.Nyeremeny);
+Console.WriteLine($"4. feladat: A versenyzők átlagosan {atlag:F2} font bevételre tettek szert.");
+
+// 5. feladat: Legjobban kereső kínai játékos
+var kinaiJatekosok = versenyzok.Where(v => v.Orszag == "Kína");
+var legjobbKinai = kinaiJatekosok.OrderByDescending(v => v.Nyeremeny).First();
+Console.WriteLine("5. feladat: A legjobban kereső kínai játékos:");
+Console.WriteLine($"\tNév: {legjobbKinai.Nev}");
+Console.WriteLine($"\tHelyezés: {legjobbKinai.Helyezes}");
+Console.WriteLine($"\tNyeremény: {legjobbKinai.Nyeremeny * 380} Ft");
+
+// 6. feladat: Norvég játékos
+bool vanNorveg = versenyzok.Any(v => v.Orszag == "Norvégia");
+if (vanNorveg)
+{
+    Console.WriteLine("6. feladat: Van norvég játékos a világranglistán.");
+}
+else
+{
+    Console.WriteLine("6. feladat: Nincs norvég játékos a világranglistán.");
+}
+
+// 7. feladat: Statisztika országok szerint
+Console.WriteLine("7. feladat: Statisztika");
+var orszagStatisztika = versenyzok
+    .GroupBy(v => v.Orszag)
+    .Where(g => g.Count() > 4)
+    .OrderByDescending(g => g.Count());
+
+foreach (var orszag in orszagStatisztika)
+{
+    Console.WriteLine($"\t{orszag.Key} - {orszag.Count()} fő");
+}
---
+++ 17 - IO feladatok/13 - Snooker/Snooker/Snooker/Snooker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snooker;
+
+public class Snooker
+{
+    public int Helyezes { get; set; }
+    public string Nev { get; set; }
+    public string Orszag { get; set; }
+    public int Nyeremeny { get; set; }
+
+    public Snooker(string sor)
+    {
+        string[] adatok = sor.Split(';');
+        Helyezes = int.Parse(adatok[0]);
+        Nev = adatok[1];
+        Orszag = adatok[2];
+        Nyeremeny = int.Parse(adatok[3]);
+    }
+}
