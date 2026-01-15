73c8dd42adb6bf9ace92e6ee8d3c7aa013c767b3
Karasz Máté <b1karmat@vasvari.org>
2026-01-15 09:03:03 +0100

    2026. 01. 15. - dsadsa

---
+++ 02_Authentification-complete/Solution.WebAPI/Controllers/SecurityController.cs
@@ -0,0 +1,31 @@
+﻿namespace Solution.WebAPI.Controllers;
+
+[ApiController]
+[ProducesResponseType(statusCode:400, type: typeof(BadRequestObjectResult))]
+[ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.InternalServerError)]
+public class SecurityController(ISecurityService securityService) : ControllerBase
+{
+    [HttpPost]
+    [Route("api/security/register")]
+    [ProducesResponseType(type:typeof(Success), statusCode:200)]
+    public async Task<IActionResult> RegisterAsync([FromBody] [Required] RegisterRequestModel model)
+    {
+        var result = await securityService.RegisterAsync(model);
+        return result.Match(
+          value => Ok(value),
+          errors => errors.ToProblemResult()
+        );
+    }
+
+    [HttpPost]
+    [Route("api/security/login")]
+    [ProducesResponseType(type:typeof(Success), statusCode:200)]
+    public async Task<IActionResult> LoginAsync([FromBody] [Required] LoginRequestModel model)
+    {
+        var result = await securityService.LoginAsync(model);
+        return result.Match(
+          value => Ok(value),
+          errors => errors.ToProblemResult()
+        );
+    }
+}
