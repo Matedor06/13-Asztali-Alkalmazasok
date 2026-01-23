b9e8a4376676cc7f6e6e665e836ae24de2003a48
Karasz Máté <b1karmat@vasvari.org>
2026-01-23 08:46:03 +0100

    2026. 01. 23. -

---
+++ weatherforecast/weatherforecast/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Buffers.Text;
+using System.Text;
+using weatherforecast;
+
+var fileData = await File.ReadAllLinesAsync("weather.txt", Encoding.UTF8);
+var weather = new List<Weather>();
+foreach (var line in fileData)
+{
+    var data = line.Split(' ');
+    weather.Add(new Weather
+    {
+        CityId = data[0],
+        Time = $"{data[1].Substring(0, 2)}:{data[1].Substring(2, 2)}",
+        Direction = data[2].Substring(0, 3),
+        Speed = data[2].Substring(3),
+        Temperature = int.Parse(data[3])
+    });
+}
+
+Console.WriteLine("2. feladat");
+Console.Write("Adja meg egy település kódját! Település: ");
+string cityId = Console.ReadLine();
+var lastmeasurementofTheCity = weather.Where(x => x.CityId == cityId).OrderByDescending(x => x.Time).Select(x => x.Time).FirstOrDefault();
+Console.WriteLine($"Az utolsó mérési adat a megadott településről {lastmeasurementofTheCity}-kor érkezett.");
+Console.WriteLine("3. feladat");
+
+var leastTemperatureCity = weather.MinBy(x => x.Temperature);
+Console.WriteLine($"A legalacsonyabb hőmérséklet: {leastTemperatureCity.CityId} {leastTemperatureCity.Time} {leastTemperatureCity.Temperature} fok.");
+
+
---
+++ weatherforecast/weatherforecast/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherforecast;
+
+public class Weather
+{
+    public string CityId { get; set; }
+
+    public string Time { get; set; }
+
+    public string Direction { get; set; }
+
+    public string Speed { get; set; }
+
+    public int Temperature { get; set; }
+}
