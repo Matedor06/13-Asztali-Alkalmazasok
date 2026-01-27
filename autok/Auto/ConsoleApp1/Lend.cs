48eabe1c5a1bd664834e1afa9472b8a83faacf20
Karasz Máté <b1karmat@vasvari.org>
2026-01-27 10:39:31 +0100

    2026. 01. 27. - in progress of auto

---
+++ autok/Auto/ConsoleApp1/Lend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1;
+
+public class Lend
+{
+    public int day { get; set; }
+
+    public string dayTime { get; set; }
+
+    public string registrationNumber { get; set; }
+
+    public int employeeNumber { get; set; }
+    
+    public int kmCounter { get; set; }
+
+    public int inOut { get; set; }
+
+}
---
+++ autok/Auto/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ConsoleApp1;
+using System.Text;
+
+var fileData = await File.ReadAllLinesAsync("autok.txt", Encoding.UTF8);
+var lend = new List<Lend>();
+foreach(var line in fileData)
+{
+    var data = line.Split(' ');
+    lend.Add(new Lend
+    {
+        day = int.Parse(data[0]),
+        dayTime = data[1],
+        registrationNumber = data[2],
+        employeeNumber = int.Parse(data[3]),
+        kmCounter = int.Parse(data[4]),
+        inOut = int.Parse(data[5]),
+    });
+}
+var lastLend = lend.MaxBy(x => x.day);
+Console.WriteLine($"{lastLend.day}. nap rendszám : {lastLend.registrationNumber}");
+Console.WriteLine("szia");
+
+Console.WriteLine("Nap:");
+int day = int.Parse(Console.ReadLine());
+var specificDay = lend.Where(x => x.day == day);
+Console.WriteLine(specificDay);
