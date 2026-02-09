926d0b94ec9cf8f5f31c039977f1d4ba9af3ae0c
Karasz Máté <k6mate@gmail.com>
2026-02-09 21:40:50 +0100

    02/09/2026 - ios

---
+++ 17 - IO feladatok/37 - Hivasok/Hivasok/Hivasok/Call.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hivasok;
+
+public class Call
+{
+    public int StartHour { get; set; }
+    public int StartMinute { get; set; }
+    public int StartSecond { get; set; }
+    public int EndHour { get; set; }
+    public int EndMinute { get; set; }
+    public int EndSecond { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public bool IsMobile()
+    {
+        string prefix = PhoneNumber.Substring(0, 2);
+        return prefix == "39" || prefix == "41" || prefix == "71";
+    }
+
+    public int GetBilledMinutes()
+    {
+        int startInSeconds = StartHour * 3600 + StartMinute * 60 + StartSecond;
+        int endInSeconds = EndHour * 3600 + EndMinute * 60 + EndSecond;
+        int durationInSeconds = endInSeconds - startInSeconds;
+        
+        // Minden megkezdett perc egy egész percnek számít
+        int minutes = durationInSeconds / 60;
+        if (durationInSeconds % 60 > 0)
+        {
+            minutes++;
+        }
+        
+        return minutes;
+    }
+
+    public bool IsPeakTime()
+    {
+        // Csúcsidő 7:00:00 - 18:00:00
+        int startInSeconds = StartHour * 3600 + StartMinute * 60 + StartSecond;
+        int peakStart = 7 * 3600; // 7:00:00
+        int peakEnd = 18 * 3600;  // 18:00:00
+        
+        return startInSeconds >= peakStart && startInSeconds < peakEnd;
+    }
+
+    public double GetCost()
+    {
+        int minutes = GetBilledMinutes();
+        bool isPeak = IsPeakTime();
+        bool isMobile = IsMobile();
+
+        double ratePerMinute;
+        
+        if (isMobile)
+        {
+            ratePerMinute = isPeak ? 69.175 : 46.675;
+        }
+        else
+        {
+            ratePerMinute = isPeak ? 30 : 15;
+        }
+
+        return minutes * ratePerMinute;
+    }
+
+    public override string ToString()
+    {
+        return $"{StartHour} {StartMinute} {StartSecond} {EndHour} {EndMinute} {EndSecond} {PhoneNumber}";
+    }
+}
